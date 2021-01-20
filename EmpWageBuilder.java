3210299af5952984db6ccf395d9e9b3632d1e3b9
kaushiki <kaushiki.ak88@gmail.com>
2021-01-20 11:21:46 +0530

    Employees present or abscent

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,5 +1,13 @@
 public class EmpWageBuilder{
 	public static void main(String[] args){
-System.out.println("Welcome to Employee Wage Builder");
+		System.out.println("Welcome to Employee Wage Builder");
+		// contents
+		int is_Full_Time = 1;
+			//computation
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		if (empCheck == is_Full_Time)
+			System.out.println("Employee is present ");
+		else
+			System.out.println("Employee is Abscent");
 	}
 }
