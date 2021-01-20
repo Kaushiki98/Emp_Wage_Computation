051f59a245d7af0d3da643a34ee2690869af0e6b
kaushiki <kaushiki.ak88@gmail.com>
2021-01-20 13:44:37 +0530

    employee wage for 1 month

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -3,24 +3,18 @@ public class EmpWageBuilder{
 		System.out.println("Welcome to Employee Wage Builder");
 		// contents
 		int is_Full_Time = 1;
-		int is_Part_Time = 2;
 		int Wage_per_hour = 20;
-		int no_of_hours = 8;
-		int no_of_part_time_hour = 4;
-		int month = 20;
+		int no_of_hours = 100;
+		int days = 20;
 		//computation
 		int empCheck =(int) (Math.floor(Math.random() * 10) % 2);
-		switch (empCheck){
-		case 0:
+		if(empCheck == is_Full_Time){
 			System.out.println("Employee is present ");
-			int Payment = Wage_per_hour * no_of_hours * month;
+			int Payment = Wage_per_hour * no_of_hours * days;
 			System.out.println("Full time Payment for one month: "+ Payment);
-			break;
-		case 1:
-			System.out.println("Employee is part time");
-			int Payments = Wage_per_hour * no_of_part_time_hour * month;
-			System.out.println("Part time Payment for one month: "+ Payments);
-			break;
+		}
+		else{
+			System.out.println("Employee is Abscent");
 		}
 	}
 }
