7a9d480d518fa3f9384f86a8c937a5c52455a4f5
kaushiki <kaushiki.ak88@gmail.com>
2021-01-20 11:56:36 +0530

    employee wage in hours

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -3,11 +3,18 @@ public class EmpWageBuilder{
 		System.out.println("Welcome to Employee Wage Builder");
 		// contents
 		int is_Full_Time = 1;
-			//computation
+		int Wage_per_hour = 20;
+		int no_of_hours = 8;
+		//computation
 		double empCheck = Math.floor(Math.random() * 10) % 2;
-		if (empCheck == is_Full_Time)
+		if (empCheck == is_Full_Time){
 			System.out.println("Employee is present ");
-		else
+			int Payment = Wage_per_hour * no_of_hours;
+			System.out.println("Payment is: "+ Payment);
+		}
+		else{
 			System.out.println("Employee is Abscent");
+			System.out.println("Payment is 0");
+		}
 	}
 }
